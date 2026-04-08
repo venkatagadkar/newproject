47c737abbcc49c5ca74fcabf38042fcffa7584ed
Venkat <venkatsaikiran58@gmail.com>
2026-04-08 12:19:29 +0530

    ok to push

---
+++ venkat.py
@@ -0,0 +1,3 @@
+venkat's first project on GitHub
+# How to enable the GitHub or git to manage the local folers or files.
+First we have to use - git init
---
+++ vsk.py
@@ -0,0 +1,2 @@
+this is the new project im doing in git hub
+
